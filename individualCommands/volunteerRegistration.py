a7a00f4456dd027fc8039ed151fac9aa4f8da181
morpheuschoo <morpheuschoo@gmail.com>
2026-02-05 23:45:53 +0800

    Change volunteer confirmation message

--- individualCommands/volunteerRegistration.py
+++ individualCommands/volunteerRegistration.py
@@ -59,7 +59,10 @@ async def volunteerRegistration_SECOND(update: Update, context: ContextTypes.DEF
     with open('data/volunteerDetails.json', 'w') as file:
         dump(list(volunteerSet), file, indent = 1)
 
-    await context.bot.send_message(chatID, f"Thank you {userDict[str(chatID)]['fullName']}! We have signed you up as a volunteer.", reply_markup = ReplyKeyboardRemove())
+    await context.bot.send_message(chatID,
+                                   f"Thank you {userDict[str(chatID)]['fullName']} for signing up as a volunteer! 🥰\
+                                     \n\nPlease join this group chat for any updates an announcements related to the CAPT Care Pal bot: https://t.me/+sRLGvHKfqu01OThl",
+                                   reply_markup = ReplyKeyboardRemove())
 
     return ConversationHandler.END
 
